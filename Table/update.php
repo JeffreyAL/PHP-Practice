dcf38445065853cb77b94431d99a0655cbb64f0b
Jeffrey A <jalhassan@umassd.edu>
2017-01-02 11:53:12 -0500

    few changes on update.php

--- Table/update.php
+++ Table/update.php
@@ -1,16 +1,18 @@
 <?php
-
+// action when update is pressed
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
+  // collect user data
   $id = $_POST['id'];
   $name = $_POST['name'];
   $cc = $_POST['cc'];
   $district = $_POST['district'];
   $population = $_POST['population'];
 
+  // update the data on the database
   $sql = "UPDATE city SET ID = '$id', Name = '$name', CountryCode = '$cc',
   District = '$district', Population = '$population' WHERE ID=$id ";
-
+  // run the update on the database
   if(mysqli_query($connect, $sql)){
     echo "Update was successful";
     echo "<a href='table.php'> Click me to complete update</a>";
